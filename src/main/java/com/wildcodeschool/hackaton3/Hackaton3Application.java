525e5818a5170e55d2bd7cf75bd0f45db8812e43
52-28 <aymeric.feillault@gmail.com>
2019-12-18 17:26:29 +0100

    initialisation

---
+++ src/main/java/com/wildcodeschool/hackaton3/Hackaton3Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.hackaton3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackaton3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/hackaton3/Hackaton3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.hackaton3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
