7c2607621dbf96d3bdca1d4ac75d65333a09eead
52-28 <aymeric.feillault@gmail.com>
2019-12-18 18:28:15 +0100

    creation reward

--- src/main/java/com/wildcodeschool/hackaton3/entities/Reward.java
+++ src/main/java/com/wildcodeschool/hackaton3/entities/Reward.java
@@ -1,5 +1,62 @@
 package com.wildcodeschool.hackaton3.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Reward {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String titre;
+	private String montant;
+	private String code;
+
+
+	public Reward() {
+		
+	};
+	
+	
+	public Reward(String titre, String montant, String code) {
+		super();
+		this.titre = titre;
+		this.montant = montant;
+		this.code = code;
+	}
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+
+	public String getMontant() {
+		return montant;
+	}
+
+
+	public void setMontant(String montant) {
+		this.montant = montant;
+	}
+
+
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
 
 }
