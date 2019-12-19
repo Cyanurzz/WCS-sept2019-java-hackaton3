ccae41bffcb026cc6f10483dee4026d0172a183e
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-12-19 17:56:22 +0100

    recup données json + convertion jsonNode vers type utilisable

--- src/main/java/com/wildcodeschool/hackaton3/controllers/Controllers.java
+++ src/main/java/com/wildcodeschool/hackaton3/controllers/Controllers.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import com.wildcodeschool.hackaton3.repos.UserRepository;
 import com.wildcodeschool.hackaton3.services.ConnectApi;
 
---
+++ src/main/java/com/wildcodeschool/hackaton3/services/Coordinates.java
@@ -0,0 +1,30 @@
+package com.wildcodeschool.hackaton3.services;
+
+public class Coordinates {
+
+	private double lat;
+	private double lon;
+	
+	public Coordinates() {
+		
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	
+	
+	
+}
