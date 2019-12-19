c9456e6ae167a6359b9bd2dc9c5a87303f1d5834
52-28 <aymeric.feillault@gmail.com>
2019-12-20 00:55:19 +0100

    bullshits

--- src/main/resources/static/testAssist.js
+++ src/main/resources/static/testAssist.js
@@ -31,7 +31,7 @@ var TxtType = function(el, toRotate, period) {
         } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 20000;
+        delta = 500;
         }
 
         setTimeout(function() {
@@ -47,10 +47,31 @@ var TxtType = function(el, toRotate, period) {
             if (toRotate) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
+            
         }
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
+        doit();
     };
+    
+    
+    function flicker(){
+    	  $("#xp-increase-fx-flicker").css("opacity", "1");
+    	  $("#xp-increase-fx-flicker").animate({"opacity":Math.random()}, 100, flicker);
+    	}
+
+    	$(document).ready(function(){
+    	  flicker();
+    	});
+
+    	function doit(){
+    	  $("#xp-increase-fx").css("display","inline-block");
+    	  $("#xp-bar-fill").css("box-shadow",/*"0px 0px 15px #06f,*/ "-5px 0px 10px #fff inset");
+    	  setTimeout(function(){$("#xp-bar-fill").css("-webkit-transition","all 2s ease");
+    	  $("#xp-bar-fill").css("width","75%");},100);
+    	  setTimeout(function(){$("#xp-increase-fx").fadeOut(500);$("#xp-bar-fill").css({"-webkit-transition":"all 0.5s ease","box-shadow":""});},2000);
+    	 
+    	}
